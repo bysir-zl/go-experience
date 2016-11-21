42f23b333543d66c842c2d5c41088c1bf4eab10c
bysir <1019654929@qq.com>
2016-11-21 09:28:44 +0800

    update

--- tree/tree_test.go
+++ tree/tree_test.go
@@ -7,43 +7,58 @@ import (
 
 type Node struct {
 	value int
-	Child []*Node
+	Child []Node
 }
 
 func TestTree(t *testing.T) {
-	root := &Node{value:0}
-	node1 := &Node{value:1}
-	node2 := &Node{value:2}
-	node3 := &Node{value:3}
-	root.Child = []*Node{node1, node2, node3}
+	root := Node{value:0}
+	node1 := Node{value:1}
+	node2 := Node{value:2}
+	node3 := Node{value:3}
+	root.Child = []Node{node1, node2, node3}
+	route := []*Node{}
 
-	route := pathOne{}
-	treeRoot(root, route)
+	p:=P{}
+	p.root = root
+	p.path = map[int][]*Node{}
+	p.treeRoot(p.root,route)
+	log.Printf("%v", p.path[1][1].value)
 
-	log.Printf("%v", path[2][1].value)
+	p2:=P2{}
+	p2.root = &root
+	p2.path = map[int][]*Node{}
+	p2.treeRoot(p2.root,route)
+	log.Printf("%v", p2.path[1][1].value)
 
 }
 
-type pathOne []*Node
-var path map[int]pathOne = map[int]pathOne{}
+type P struct {
+	path  map[int][]*Node
+	root Node
+}
+func ( p *P)treeRoot(root Node, route []*Node) {
+	route = append(route, &root)
+	if root.Child != nil {
+		for _, v := range root.Child {
+			p.treeRoot(v, route)
+		}
+	} else {
+		p.path[root.value] = route
+	}
+}
 
-func treeRoot(root *Node, route pathOne) {
+
+type P2 struct {
+	path  map[int][]*Node
+	root *Node
+}
+func ( p *P2)treeRoot(root *Node, route []*Node) {
 	route = append(route, root)
 	if root.Child != nil {
 		for _, v := range root.Child {
-			treeRoot(v, route)
+			p.treeRoot(&v, route)
 		}
 	} else {
-		if path[root.value] == nil {
-			path[root.value] = pathOne{}
-		}
-
-		// u should copy the slice , then saved ;
-		route3:=make(pathOne,len(route))
-		copy(route3,route)
-		path[root.value] = route3
-
-		// if u not, u 
-		//path[root.value] = route
+		p.path[root.value] = route
 	}
 }
