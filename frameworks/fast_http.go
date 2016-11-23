0ce3b646ffdc8fa14032cbd838bc06025899e1b3
bysir <1019654929@qq.com>
2016-11-23 11:56:57 +0800

    code

--- frameworks/fast_http.go
+++ frameworks/fast_http.go
@@ -2,6 +2,23 @@ package frameworks
 
 // huh... args.visitAll(func)?,
 // too hard to use
-func Args2MapString() {
+func Arg2Map(args *fasthttp.Args) map[string]string {
+	set := map[string]string{}
+	args.VisitAll(func(key []byte, value []byte) {
+		set[string(key)] = util.B2S(value)
+	})
 
+	return set
+}
+func Map2Arg(mapper map[string]string) *fasthttp.Args {
+	args2 := fasthttp.Args{}
+	for k, v := range mapper {
+		args2.Add(k, v)
+	}
+	return &args2
+}
+
+// simplest
+func GetPostAll(p *routing.Context) map[string]string {
+	return Arg2Map(p.Request.PostArgs())
 }
--- frameworks/fast_http_routing.go
+++ frameworks/fast_http_routing.go
@@ -1,9 +1,30 @@
 package frameworks
 
 // this router is not has handler like Controller,
-// sometime we use Controller to Simplify ours code.
-func RouterContoller() {
+// sometime we need use Controller to Simplify ours code.
+func RouterController(r *routing.RouteGroup, path string, controller interface{}, handlers ...routing.Handler) *routing.RouteGroup {
+	stru := reflect.ValueOf(controller)
+	typ := stru.Type()
+	c := r.Group(path)
+	// get all func from controller, then add it to router group if sign is 'router.handler'
+	for i := stru.NumMethod() - 1; i >= 0; i-- {
+		fun := stru.Method(i)
+		ifun, ok := fun.Interface().(func(*routing.Context) error)
 
+		if ok {
+			name := typ.Method(i).Name
+			// to lower the initial
+			name = strings.ToLower(string(name[0])) + name[1:]
+			c.Any("/" + name, func(p *routing.Context) error {
+				p.Set("method", name)
+				return ifun(p)
+			}, handlers...)
+		}
+	}
+	return c
 }
 
 
+// i think the routing is not perfect :
+// 1. 'Use' like Middleware but is not, it can handle incoming request, but can not handle outcoming response 
+// 2. when i have too many router group, the code format is not beautiful. is better use index format?
